255f43b799383e2e57bc0c9a1f2a5211fae34f9b
Oleksii Aleshchenko <aleshchenko.oleksii@lll.kpi.ua>
2024-02-16 14:50:30 +0200

    add Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        System.out.println(args[3]);
+    }
+}
