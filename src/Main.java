6527d6ffc6194dad71e28cbde96f96e3a317ea37
Oleksii Aleshchenko <aleshchenko.oleksii@lll.kpi.ua>
2024-02-23 13:28:29 +0200

    update Main.java with 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!3");
         System.out.println(args[0]);
         System.out.println(args[1]);
         System.out.println(args[2]);
