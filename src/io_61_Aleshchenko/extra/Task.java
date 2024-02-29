8751bcb1bb056f760bb2f4897419c7abc8bf55e0
Oleksii Aleshchenko <aleshchenko.oleksii@lll.kpi.ua>
2024-02-29 16:02:13 +0200

    add interesting tasks

---
+++ src/io_61_Aleshchenko/extra/Task.java
@@ -0,0 +1,20 @@
+package io_61_Aleshchenko.extra;
+
+public class Task {
+    public static void main(String[] args) {
+//        ====================
+        int n = 20;
+        for (int i = 0; i < n; i--) {
+            print("*");
+        }
+        //=====================
+        int a = 1;
+        if (a==n) {
+
+        }
+    }
+
+    private static void print(String s) {
+        System.out.print(s);
+    }
+}
