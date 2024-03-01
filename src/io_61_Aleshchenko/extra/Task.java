04050897f832b1163d0eca52f82eab76a8ef2e04
Oleksii Aleshchenko <aleshchenko.oleksii@lll.kpi.ua>
2024-03-01 12:44:17 +0200

    update interesting tasks

--- src/io_61_Aleshchenko/extra/Task.java
+++ src/io_61_Aleshchenko/extra/Task.java
@@ -2,16 +2,17 @@ package io_61_Aleshchenko.extra;
 
 public class Task {
     public static void main(String[] args) {
-//        ====================
+//        ==================== Завдання: змінити(!) один(!) символ, щоб вивелося 20 зірочок
+//        (в Java - 2 розв'язки, а в С/С++ - 3 розв'язки)
         int n = 20;
         for (int i = 0; i < n; i--) {
             print("*");
         }
         //=====================
-        int a = 1;
+        /*int a = 1;
         if (a==n) {
 
-        }
+        }*/
     }
 
     private static void print(String s) {
--- src/io_61_Aleshchenko/extra/Task2.java
+++ src/io_61_Aleshchenko/extra/Task2.java
@@ -1,16 +1,8 @@
 package io_61_Aleshchenko.extra;
-
+// Завдання: що виведе данна програма?
 public class Task2 {
     public static void main(String[] args) {
         http://google.com
-        /*http:for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.println(i);
-                System.out.println(j);
-                System.out.println();
-                break http;
-            }
-        }*/
         System.out.print("!!!");
     }
 
