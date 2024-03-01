d3142abc704274b026f49e363171aa55a61e4114
Oleksii Aleshchenko <aleshchenko.oleksii@lll.kpi.ua>
2024-03-01 14:13:53 +0200

    update Lab1

--- src/io_61_Aleshchenko/Lab1.java
+++ src/io_61_Aleshchenko/Lab1.java
@@ -1,7 +1,44 @@
 package io_61_Aleshchenko;
 
 public class Lab1 {
+//    #6101
+//    O1 = 1 (-)
+//    C = 2
+//    O2 = 1 (/)
+//    тип i та j: char
+
     public static void main(String[] args) {
+//        todo user input
+//        todo assign int for char variable
+//        final int a = 1;
+
+        final char a = 3;
+        final char b = 1;
+        final char n = 4;
+        final char m = 2;
+        final char C = 2;
+//        final char a = '1';
+//        System.out.print("111");
+//        ++a;
+//        System.out.println(a++);
+//        System.out.println((int) a);
+
+        System.out.println(Integer.parseInt("1"));
+
+        double d = (double) 1 / 2;
+        System.out.println(d);
+        System.out.printf("%f", (double) 1 / 2);
+        System.out.println();
+        double s = 0;
+        for (int i = a; i <= n; ++i) {
+            for (int j = b; j <= m; j++) {
+                s += (double) i / j / (i - C);
+            }
+        }
+        System.out.println(s);
+//        System.out.print("222");
+
+
         System.out.println("Done!");
     }
 }
