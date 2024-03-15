00e8f56322254bdf053c3ba29e6b227805ec0edb
Oleksii Aleshchenko <aleshchenko.oleksii@lll.kpi.ua>
2024-03-15 13:25:05 +0200

    update Lab1

--- src/io_61_Aleshchenko/Lab1.java
+++ src/io_61_Aleshchenko/Lab1.java
@@ -13,7 +13,7 @@ public class Lab1 {
 //        final int a = 1;
 
         final char a = 3;
-        final char b = 1;
+        final char b = 0;
         final char n = 4;
         final char m = 2;
         final char C = 2;
@@ -23,16 +23,22 @@ public class Lab1 {
 //        System.out.println(a++);
 //        System.out.println((int) a);
 
-        System.out.println(Integer.parseInt("1"));
+//        System.out.println(Integer.parseInt("1"));
 
-        double d = (double) 1 / 2;
+        /*double d = (double) 1 / 2;
         System.out.println(d);
         System.out.printf("%f", (double) 1 / 2);
-        System.out.println();
+        System.out.println();*/
         double s = 0;
         for (int i = a; i <= n; ++i) {
             for (int j = b; j <= m; j++) {
-                s += (double) i / j / (i - C);
+                if (j != 0 && i - C != 0) {
+                    s += (double) i / j / (i - C);
+                } else {
+                    System.out.println("Error: division by zero");
+//                    todo use return
+                    break;
+                }
             }
         }
         System.out.println(s);
