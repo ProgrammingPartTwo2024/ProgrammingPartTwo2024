0388ec31f12ec62719b578d0fdacb8db7681540d
Oleksii Aleshchenko <aleshchenko.oleksii@lll.kpi.ua>
2024-04-19 13:21:21 +0300

    update Lab3

---
+++ src/io_61_Aleshchenko/Lab3.java
@@ -0,0 +1,39 @@
+package io_61_Aleshchenko;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Lab3 {
+    public static void main(String[] args) {
+
+//        В усіх питальних реченнях заданого тексту знайти та надрукувати без повторень слова заданої довжини.
+//        Якщо таких слів немає, то вивести відповідне повідомлення.
+
+        /*String text = "В усіх питальних реченнях заданого тексту знайти та надрукувати без повторень слова заданої довжини. Якщо таких слів немає, то вивести відповідне повідомлення.";
+        String[] sentences = text.split("[.!?]");
+        int wordLength = 5;
+
+        for (String sentence : sentences) {
+            String[] words = sentence.split("[\\s,;:.!?]+");
+            for (String word : words) {
+                if (word.length() == wordLength) {
+                    System.out.println(word);
+                }
+            }
+        }*/
+
+        StringBuilder stringBuilder = new StringBuilder("Мій текст");
+//        stringBuilder.toString().matches("\\b[^ ]{2}\\b", )
+//        Pattern.matches()
+        int wordLength = 5;
+
+//        Pattern pattern = Pattern.compile("\\b\\w{" + wordLength + "}\\b");
+        Pattern pattern = Pattern.compile("\\b[^ ]{" + wordLength + "}\\b");
+
+        Matcher matcher = pattern.matcher(stringBuilder);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
